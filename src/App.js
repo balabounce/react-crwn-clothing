9bf59eb2e08a92b93e6878b7d46eb34b0cf7084b
Balabounce <yoba.number.one@gmail.com>
2021-07-24 12:49:46 +0300

    implemented firebase utils, inluded ability to store authenetiated users into firebase database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HomePage } from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 export default class App extends React.Component {
   state = {
@@ -15,9 +15,26 @@ export default class App extends React.Component {
   unsubcribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+      else {
+        this.setState({
+          currentUser: userAuth 
+        })
+      }
     });
   }
 
